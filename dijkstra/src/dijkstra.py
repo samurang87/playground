dcf77f48f93215f8eabd7dce72af7c99ac46d1e5
samurang87 <2512470+samurang87@users.noreply.github.com>
2019-06-10 20:15:00 +0200

    dijkstra WIP

--- dijkstra/src/dijkstra.py
+++ dijkstra/src/dijkstra.py
@@ -1,12 +1,13 @@
 import sys
+from collections import OrderedDict
 
 
 class Node:
-
     def __init__(self, data: int):
 
         self.data = data
         self.edges = dict()  # node val: distance
+        self.visited = False
         self.total_distance = None
 
     def __str__(self):
@@ -16,17 +17,13 @@ class Node:
 def create_graph(start_val, input_list):
 
     graph = dict()
-
     start_node = Node(data=start_val)
-
     graph[start_val] = start_node
 
     for index, edge in enumerate(input_list):
-
         nodes = sorted(edge[:2])
 
         for n in nodes:
-
             if n not in graph.keys():
                 graph[n] = Node(data=n)
 
@@ -38,16 +35,57 @@ def create_graph(start_val, input_list):
     return graph
 
 
+def calculate_shortest_distance(start_val, graph, current=None):
+
+    priority_q = list()
+
+    if current is None:
+        current = graph[start_val]
+        current.total_distance = 0
+
+    # for each node at the end of the edges
+    for edge in current.edges.keys():
+
+        if graph[edge].visited is False:
+
+            # add the distance to the current node to the "distance" property,
+            # if the total distance is higher or not None
+            distance_from_current = current.edges[edge]
+            if (
+                graph[edge].total_distance is None
+                or graph[edge].total_distance
+                > current.total_distance + distance_from_current
+            ):
+                graph[edge].total_distance = current.total_distance + distance_from_current
+
+            # add the nodes at the end of the edges to the priority list
+            priority_q.append(graph[edge])
+
+    current.visited = True
+
+    all_visited = True
+    for node in graph.keys():
+        if graph[node].visited is False:
+            all_visited = False
+
+    if all_visited:
+        return graph
+
+    if len(priority_q) > 0:
+        # sort by distance
+        priority_q.sort(key=lambda x: x.total_distance)
+
+        for node in priority_q:
+            calculate_shortest_distance(start_val=start_val, current=node, graph=graph)
+
+
 if __name__ == "__main__":
 
     input = []
 
     for line in sys.stdin:
-
         input.append([int(n) for n in str.split(line)])
 
     n_test = input.pop(0)
-
     nodes_and_edges = input.pop(0)
-
     start = input.pop(-1)
--- dijkstra/test/test_dijsktra.py
+++ dijkstra/test/test_dijsktra.py
@@ -1,13 +1,32 @@
 from unittest import TestCase
 
-from dijkstra.src.dijkstra import create_graph, Node
+from dijkstra.src.dijkstra import create_graph, Node, calculate_shortest_distance
+
+input_example = [[1, 2, 24], [1, 4, 20], [3, 1, 3], [4, 3, 12]]
 
 
 class TestDijsktra(TestCase):
-
     def test_create_graph_simple(self):
 
         graph = create_graph(start_val=1, input_list=[[2, 1, 10]])
-
         self.assertEqual({2: 10}, graph[1].edges)
-mmi
+
+    def test_create_graph_as_example(self):
+
+        graph = create_graph(start_val=1, input_list=input_example)
+        self.assertEqual(4, len(graph.keys()))
+
+    def test_shortest(self):
+
+        expected = [(2, 24), (3, 3), (4, 15)]
+        graph = create_graph(start_val=1, input_list=input_example)
+
+        graph_calculated = calculate_shortest_distance(1, graph=graph)
+
+        res = []
+        for val in graph_calculated.keys.sort():
+            if val != 1:
+                res.append((val, graph[val].total_distance))
+
+        self.assertEqual(expected, res)
+
